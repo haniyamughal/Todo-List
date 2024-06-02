1cd2e105852873580f16c746f2fd93a3fb1cdc6c
haniyamughal <160358992+haniyamughal@users.noreply.github.com>
2024-06-02 15:06:00 +0500

    Add files via upload
    // adding a Todo-List project

---
+++ main.ts
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todoList = [];
+let conditions = true;
+
+console.log(chalk.magenta.bold("\n \t Welcome to Todo-List Application by - Haniya\n"));
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.green("Enter your New Task :"),
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-List successfully`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "False",
+        }
+    ]);
+    conditions = addMoreTask.addmore
+}
+console.log("Your updated Todo-List:" , todoList);
